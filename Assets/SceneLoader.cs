e2df1ef399112b695fa6714fe87c050ebe596beb
DESKTOP-HCMRCN5\mc-tc <tomcolin.horn@gmail.com>
2018-07-28 01:02:26 +0200

    Imported several assets, added splash screen, sounds, updated terrain and added spaceship

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+	int currentScene;
+	int maxScene;
+	[SerializeField] float loadLevelDelay = 3;
+
+	private void Awake()
+	{
+		DontDestroyOnLoad(gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+		Invoke("LoadFirstLevel", loadLevelDelay);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void LoadFirstLevel ()
+	{
+		currentScene = SceneManager.GetActiveScene().buildIndex;
+		maxScene = SceneManager.sceneCountInBuildSettings - 1;
+
+		SceneManager.LoadScene(currentScene + 1);
+		if (currentScene > maxScene)
+		{
+			currentScene = maxScene;
+		}
+	}
+}
