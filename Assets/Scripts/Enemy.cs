5729e7a76b26152698bb3c61b61ec0928c1d0f17
DESKTOP-HCMRCN5\mc-tc <tomcolin.horn@gmail.com>
2018-07-31 22:41:23 +0200

    Made enemies blow up

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	[SerializeField] ParticleSystem enemyExplosion;
+	[SerializeField] AudioSource enemyExplosionSound;
+	[SerializeField] Renderer enemyBody;
+	[SerializeField] Collider enemyCollider;
+
+	// Use this for initialization
+	void Start () {
+		enemyExplosion = GetComponent<ParticleSystem>();
+		enemyExplosionSound = GetComponent<AudioSource>();
+		enemyBody = GetComponent<Renderer>();
+		enemyCollider = GetComponent<Collider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnParticleCollision(GameObject other)
+	{
+		enemyExplosion.Play();
+		enemyExplosionSound.Play();
+		enemyBody.enabled = false;
+		enemyCollider.enabled = false;
+		print("Particles collided with enemy " + gameObject.name);
+	}
+}
